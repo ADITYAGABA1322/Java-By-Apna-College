f312bb4c1e62116cad3811a369e1a5ec97527ed3
Aditya <adityagaba1322@gmail.com>
2024-01-18 21:13:02 +0530

    add

--- AdiEndTerm.java
+++ AdiEndTerm.java
@@ -51,32 +51,43 @@
 
 // The bag's capacity is 7. By selecting Item 2 and Item 3, the total weight is 7 (2 + 5) which is equal to 7, and the total profit is 9 (4 + 5), which is the maximum possible.
 
-import java.util.*;
+// import java.util.*;
+
+// public class AdiEndTerm {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] weight = new int[n];
+//         int[] profit = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             weight[i] = sc.nextInt();
+//             profit[i] = sc.nextInt();
+//         }
+//         int capacity = sc.nextInt();
+//         int[][] dp = new int[n + 1][capacity + 1];
+//         for (int i = 0; i <= n; i++) {
+//             for (int j = 0; j <= capacity; j++) {
+//                 if (i == 0 || j == 0) {
+//                     dp[i][j] = 0;
+//                 } else if (weight[i - 1] <= j) {
+//                     dp[i][j] = Math.max(profit[i - 1] + dp[i - 1][j - weight[i - 1]], dp[i - 1][j]);
+//                 } else {
+//                     dp[i][j] = dp[i - 1][j];
+
+//                 }
+//             }
+//         }
+//         System.out.println(dp[n][capacity]);
+//     }
+// }
 
 public class AdiEndTerm {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] weight = new int[n];
-        int[] profit = new int[n];
-        for (int i = 0; i < n; i++) {
-            weight[i] = sc.nextInt();
-            profit[i] = sc.nextInt();
-        }
-        int capacity = sc.nextInt();
-        int[][] dp = new int[n + 1][capacity + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= capacity; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0;
-                } else if (weight[i - 1] <= j) {
-                    dp[i][j] = Math.max(profit[i - 1] + dp[i - 1][j - weight[i - 1]], dp[i - 1][j]);
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+        System.out.println(calculate(3, 9)); // Output: 1009
+        System.out.println(calculate(4, 1)); // Output: 10001
+    }
 
-                }
-            }
-        }
-        System.out.println(dp[n][capacity]);
+    public static String calculate(int n, int x) {
+        return String.valueOf((int) Math.pow(10, n) + x);
     }
 }
