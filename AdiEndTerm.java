a9e7c66ac3f14a058c67b601ec99a26203d84479
Aditya <adityagaba1322@gmail.com>
2024-01-25 15:20:38 +0530

    add

--- AdiEndTerm.java
+++ AdiEndTerm.java
@@ -105,184 +105,187 @@
 // Explanation:
 // For the given input, nodes 30 20 40 10 50 are inserted into the AVL tree, and the preorder traversal of the created AVL tree is 30 20 10 40 50.
 
-// import java.util.*;
+import java.util.*;
 
-// public class AdiEndTerm {
-//   static class Node {
-//     int data;
-//     Node left, right;
+class Node {
+  int data;
+  Node left, right;
 
-//     Node(int data) {
-//       this.data = data;
-//       left = right = null;
-//     }
-//   }
+  Node(int data) {
+    this.data = data;
+    left = right = null;
+  }
+}
 
-//   static class AVLTree {
-//     Node root;
+class AVLTree {
+  Node root;
 
-//     AVLTree() {
-//       root = null;
-//     }
+  AVLTree() {
+    root = null;
+  }
 
-//     int height(Node node) {
-//       if (node == null)
-//         return 0;
-//       return 1 + Math.max(height(node.left), height(node.right));
-//     }
+  int height(Node node) {
+    if (node == null)
+      return 0;
+    return 1 + Math.max(height(node.left), height(node.right));
+  }
 
-//     int getBalance(Node node) {
-//       if (node == null)
-//         return 0;
-//       return height(node.left) - height(node.right);
-//     }
+  int getBalance(Node node) {
+    if (node == null)
+      return 0;
+    return height(node.left) - height(node.right);
+  }
 
-//     Node rightRotate(Node node) {
-//       Node temp = node.left;
-//       Node temp2 = temp.right;
-//       temp.right = node;
-//       node.left = temp2;
-//       return temp;
-//     }
+  Node rightRotate(Node node) {
+    Node temp = node.left;
+    Node temp2 = temp.right;
+    temp.right = node;
+    node.left = temp2;
+    return temp;
+  }
 
-//     Node leftRotate(Node node) {
-//       Node temp = node.right;
-//       Node temp2 = temp.left;
-//       temp.left = node;
-//       node.right = temp2;
-//       return temp;
-//     }
+  Node leftRotate(Node node) {
+    Node temp = node.right;
+    Node temp2 = temp.left;
+    temp.left = node;
+    node.right = temp2;
+    return temp;
+  }
 
-//     Node insert(Node node, int data) {
-//       if (node == null)
-//         return new Node(data);
-//       if (data < node.data)
-//         node.left = insert(node.left, data);
-//       else if (data > node.data)
-//         node.right = insert(node.right, data);
-//       else
-//         return node;
-//       int balance = getBalance(node);
-//       if (balance > 1 && data < node.left.data)
-//         return rightRotate(node);
-//       if (balance < -1 && data > node.right.data)
-//         return leftRotate(node);
-//       if (balance > 1 && data > node.left.data) {
-//         node.left = leftRotate(node.left);
-//         return rightRotate(node);
-//       }
-//       if (balance < -1 && data < node.right.data) {
-//         node.right = rightRotate(node.right);
-//         return leftRotate(node);
-//       }
-//       return node;
-//     }
+  Node insert(Node node, int data) {
+    if (node == null)
+      return new Node(data);
+    if (data < node.data)
+      node.left = insert(node.left, data);
+    else if (data > node.data)
+      node.right = insert(node.right, data);
+    else
+      return node;
+    int balance = getBalance(node);
+    if (balance > 1 && data < node.left.data)
+      return rightRotate(node);
+    if (balance < -1 && data > node.right.data)
+      return leftRotate(node);
+    if (balance > 1 && data > node.left.data) {
+      node.left = leftRotate(node.left);
+      return rightRotate(node);
+    }
+    if (balance < -1 && data < node.right.data) {
+      node.right = rightRotate(node.right);
+      return leftRotate(node);
+    }
+    return node;
+  }
 
-//     Node minValueNode(Node node) {
-//       Node current = node;
-//       while (current.left != null)
-//         current = current.left;
-//       return current;
-//     }
+  Node minValueNode(Node node) {
+    Node current = node;
+    while (current.left != null)
+      current = current.left;
+    return current;
+  }
 
-//     Node deleteNode(Node root, int data) {
-//       if (root == null)
-//         return root;
-//       if (data < root.data)
-//         root.left = deleteNode(root.left, data);
-//       else if (data > root.data)
-//         root.right = deleteNode(root.right, data);
-//       else {
-//         if ((root.left == null) || (root.right == null)) {
-//           Node temp = null;
-//           if (temp == root.left)
-//             temp = root.right;
-//           else
-//             temp = root.left;
-//           if (temp == null) {
-//             temp = root;
-//             root = null;
-//           } else
-//             root = temp;
-//         } else {
-//           Node temp = minValueNode(root.right);
-//           root.data = temp.data;
-//           root.right = deleteNode(root.right, temp.data);
-//         }
-//       }
-//       if (root == null)
-//         return root;
-//       int balance = getBalance(root);
-//       if (balance > 1 && getBalance(root.left) >= 0)
-//         return rightRotate(root);
-//       if (balance > 1 && getBalance(root.left) < 0) {
-//         root.left = leftRotate(root.left);
-//         return rightRotate(root);
-//       }
-//       if (balance < -1 && getBalance(root.right) <= 0)
-//         return leftRotate(root);
-//       if (balance < -1 && getBalance(root.right) > 0) {
-//         root.right = rightRotate(root.right);
-//         return leftRotate(root);
-//       }
-//       return root;
-//     }
+  Node deleteNode(Node root, int data) {
+    if (root == null)
+      return root;
+    if (data < root.data)
+      root.left = deleteNode(root.left, data);
+    else if (data > root.data)
+      root.right = deleteNode(root.right, data);
+    else {
+      if ((root.left == null) || (root.right == null)) {
+        Node temp = null;
+        if (temp == root.left)
+          temp = root.right;
+        else
+          temp = root.left;
+        if (temp == null) {
+          temp = root;
+          root = null;
+        } else
+          root = temp;
+      } else {
+        Node temp = minValueNode(root.right);
+        root.data = temp.data;
+        root.right = deleteNode(root.right, temp.data);
+      }
+    }
+    if (root == null)
+      return root;
+    int balance = getBalance(root);
+    if (balance > 1 && getBalance(root.left) >= 0)
+      return rightRotate(root);
+    if (balance > 1 && getBalance(root.left) < 0) {
+      root.left = leftRotate(root.left);
+      return rightRotate(root);
+    }
+    if (balance < -1 && getBalance(root.right) <= 0)
+      return leftRotate(root);
+    if (balance < -1 && getBalance(root.right) > 0) {
+      root.right = rightRotate(root.right);
+      return leftRotate(root);
+    }
+    return root;
+  }
 
-//     void preOrder(Node node) {
-//       if (node != null) {
-//         System.out.print(node.data + " ");
-//         preOrder(node.left);
-//         preOrder(node.right);
-//       }
-//     }
+  void preOrder(Node node) {
+    if (node != null) {
+      System.out.print(node.data + " ");
+      preOrder(node.left);
+      preOrder(node.right);
+    }
+  }
 
-//     void printTree(Node node) {
-//       if (node != null) {
-//         System.out.println(node.data);
-//         if (node.left != null)
-//           System.out.println("Left: " + node.left.data);
-//         if (node.right != null)
-//           System.out.println("Right: " + node.right.data);
+  void printTree(Node node) {
+    if (node != null) {
+      System.out.println(node.data);
+      if (node.left != null)
+        System.out.println("Left: " + node.left.data);
+      if (node.right != null)
+        System.out.println("Right: " + node.right.data);
 
-//         printTree(node.left);
-//         printTree(node.right);
-//       }
+      printTree(node.left);
+      printTree(node.right);
+    }
 
-//     }
-//   }
+  }
+}
 
-//   public static void main(String[] args) {
-//     AVLTree tree = new AVLTree();
-//     Scanner scanner = new Scanner(System.in);
+class AdiEndTerm {
 
-//     System.out.print("Enter the number of nodes: ");
-//     int n = scanner.nextInt();
+  public static void main(String[] args) {
+    AVLTree tree = new AVLTree();
+    Scanner scanner = new Scanner(System.in);
 
-//     System.out.println("Enter the keys for the nodes:");
-//     for (int i = 0; i < n; i++) {
-//       int key = scanner.nextInt();
-//       tree.root = tree.insert(tree.root, key);
-//     }
-//     tree.printTree(tree.root);
+    System.out.print("Enter the number of nodes: ");
+    int n = scanner.nextInt();
 
-//     int deleteKey = 0;
-//     System.out.print("Enter the key to delete: ");
-//     deleteKey = scanner.nextInt();
-//     tree.root = tree.deleteNode(tree.root, deleteKey);
-//     tree.printTree(tree.root);
-//     tree.preOrder(tree.root);
+    System.out.println("Enter the keys for the nodes:");
+    for (int i = 0; i < n; i++) {
+      int key = scanner.nextInt();
+      tree.root = tree.insert(tree.root, key);
+    }
+    tree.printTree(tree.root);
 
-//   }
-// }
+    int deleteKey = 0;
+    System.out.print("Enter the key to delete: ");
+    deleteKey = scanner.nextInt();
+    tree.root = tree.deleteNode(tree.root, deleteKey);
+    tree.printTree(tree.root);
+    tree.preOrder(tree.root);
+
+  }
+}
 
 // 3. Problem Statement
-// Write a Java code to perform the Node Deletion of the Leaf Nodes form the BST. User enters the value of the nodes. 
-// Your task is to find the Left-Most Leaf Child & Right-Most Leaf child in the BST, delete them, and then print the POST Order traversal of that given Tree.
+// Write a Java code to perform the Node Deletion of the Leaf Nodes form the
+// BST. User enters the value of the nodes. 
+// Your task is to find the Left-Most Leaf Child & Right-Most Leaf child in the
+// BST, delete them, and then print the POST Order traversal of that given Tree.
 
 // Input Format
 // An array of integers (7 values)
 // Output Format
-// Post-order traversal of tree after applying deletion operation 
+// Post-order traversal of tree after applying deletion operation
 // Note: Integer array length must be equal to 7.
 // Example
 // Sample Input 1
@@ -290,98 +293,99 @@
 // Sample Output 1
 // 5 20 10
 
-// we need to find the left most and right most leaf node and delete them and print the post order traversal of the tree
+// we need to find the left most and right most leaf node and delete them and
+// print the post order traversal of the tree
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class Node {
-  int data;
-  Node left, right;
+// class Node {
+// int data;
+// Node left, right;
 
-  public Node(int item) {
-    data = item;
-    left = right = null;
-  }
-}
+// public Node(int item) {
+// data = item;
+// left = right = null;
+// }
+// }
 
-class BST {
-  Node root;
+// class BST {
+// Node root;
 
-  BST() {
-    root = null;
-  }
+// BST() {
+// root = null;
+// }
 
-  void insert(int data) {
-    root = insertRec(root, data);
-  }
+// void insert(int data) {
+// root = insertRec(root, data);
+// }
 
-  Node insertRec(Node root, int data) {
-    if (root == null) {
-      root = new Node(data);
-      return root;
-    }
+// Node insertRec(Node root, int data) {
+// if (root == null) {
+// root = new Node(data);
+// return root;
+// }
 
-    if (data < root.data) {
-      root.left = insertRec(root.left, data);
-    } else if (data > root.data) {
-      root.right = insertRec(root.right, data);
-    }
+// if (data < root.data) {
+// root.left = insertRec(root.left, data);
+// } else if (data > root.data) {
+// root.right = insertRec(root.right, data);
+// }
 
-    return root;
-  }
+// return root;
+// }
 
-  void deleteLeafNodes() {
-    root = deleteLeafNodesUtil(root);
-  }
+// void deleteLeafNodes() {
+// root = deleteLeafNodesUtil(root);
+// }
 
-  Node deleteLeafNodesUtil(Node root) {
-    if (root == null) {
-      return null;
-    }
+// Node deleteLeafNodesUtil(Node root) {
+// if (root == null) {
+// return null;
+// }
 
-    if (root.left == null && root.right == null) {
-      // This is a leaf node, delete it
-      return null;
-    }
+// if (root.left == null && root.right == null) {
+// // This is a leaf node, delete it
+// return null;
+// }
 
-    // Recursively delete leaf nodes in left and right subtrees
-    root.left = deleteLeafNodesUtil(root.left);
-    root.right = deleteLeafNodesUtil(root.right);
+// // Recursively delete leaf nodes in left and right subtrees
+// root.left = deleteLeafNodesUtil(root.left);
+// root.right = deleteLeafNodesUtil(root.right);
 
-    return root;
-  }
+// return root;
+// }
 
-  void postOrderTraversal(Node root) {
-    if (root != null) {
-      postOrderTraversal(root.left);
-      postOrderTraversal(root.right);
-      System.out.print(root.data + " ");
-    }
-  }
-}
+// void postOrderTraversal(Node root) {
+// if (root != null) {
+// postOrderTraversal(root.left);
+// postOrderTraversal(root.right);
+// System.out.print(root.data + " ");
+// }
+// }
+// }
 
-class AdiEndTerm {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+// class AdiEndTerm {
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
 
-    // Input array of integers
-    int[] values = new int[7];
-    System.out.println("Enter 7 integers:");
-    for (int i = 0; i < 7; i++) {
-      values[i] = scanner.nextInt();
-    }
+// // Input array of integers
+// int[] values = new int[7];
+// System.out.println("Enter 7 integers:");
+// for (int i = 0; i < 7; i++) {
+// values[i] = scanner.nextInt();
+// }
 
-    // Create BST and insert values
-    BST bst = new BST();
-    for (int value : values) {
-      bst.insert(value);
-    }
+// // Create BST and insert values
+// BST bst = new BST();
+// for (int value : values) {
+// bst.insert(value);
+// }
 
-    // Delete leaf nodes
-    bst.deleteLeafNodes();
+// // Delete leaf nodes
+// bst.deleteLeafNodes();
 
-    // Print post-order traversal after deletion
-    System.out.println("Post-order traversal after deletion:");
-    bst.postOrderTraversal(bst.root);
-  }
-}
+// // Print post-order traversal after deletion
+// System.out.println("Post-order traversal after deletion:");
+// bst.postOrderTraversal(bst.root);
+// }
+// }
