7055f78527696d7cc7b856be37ba236a6521f01c
Aditya <adityagaba1322@gmail.com>
2024-01-26 23:22:26 +0530

    a

--- AdiEndTerm.java
+++ AdiEndTerm.java
@@ -2936,3 +2936,26 @@
 //     return dp[n];
 //   }
 // }
+
+Question 37: Character with maximum frequency in a String
+Problem Statement
+Input a String S. Write a function that returns the character with maximum frequency. Print the character returned. In case, if no character is repeated in the input, the function must return first character of the string. If the different characters have same frequency in the input, first character encountered must be returned.
+Input Format
+String: The Input String S must be lowercase alphabets only.
+DO NOT INPUT Uppercase alphabets, numbers or alphanumeric characters.
+Output Format
+Character: For each test case, print the character with maximum frequency in a String.
+Constraints
+A string of length between 1 to 1000.
+Example
+Sample Input 1
+abbc
+Sample Output 1
+b
+Sample Input 2
+aabbcc
+Sample Output 2
+a
+Sample test case Explanation
+In the first test case, b is the character with maximum frequency 2. b is returned and printed as output.
+In the second test case a, b, c all three are of same frequency i.e 2 so first character a is encountered, returned and printed.
