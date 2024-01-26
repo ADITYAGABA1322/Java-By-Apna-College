3f037ad1ddff24555e2fdea2cef65648c8b5d99e
Aditya <adityagaba1322@gmail.com>
2024-01-26 23:18:59 +0530

    add

--- AdiEndTerm.java
+++ AdiEndTerm.java
@@ -2817,3 +2817,32 @@
 
 
 import java.util.*;
+
+public class AdiEndTerm {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    System.out.println(eggDrop(n, k));
+  }
+
+  public static int eggDrop(int n, int k) {
+    int[][] dp = new int[n + 1][k + 1];
+    for (int i = 1; i <= n; i++) {
+      dp[i][1] = i;
+    }
+    for (int i = 1; i <= k; i++) {
+      dp[1][i] = 1;
+    }
+    for (int i = 2; i <= n; i++) {
+      for (int j = 2; j <= k; j++) {
+        dp[i][j] = Integer.MAX_VALUE;
+        for (int x = 1; x <= i; x++) {
+          int res = 1 + Math.max(dp[x - 1][j - 1], dp[i - x][j]);
+          dp[i][j] = Math.min(dp[i][j], res);
+        }
+      }
+    }
+    return dp[n][k];
+  }
+}
