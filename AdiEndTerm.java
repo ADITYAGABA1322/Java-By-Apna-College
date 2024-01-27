3c08812bb128d1e9ef100fee92643b81f824432e
Aditya <adityagaba1322@gmail.com>
2024-01-27 17:34:29 +0530

    a

--- AdiEndTerm.java
+++ AdiEndTerm.java
@@ -59,35 +59,35 @@
 // 7 (2 + 5) which is equal to 7, and the total profit is 9 (4 + 5), which is
 // the maximum possible.
 
-import java.util.*;
+// import java.util.*;
 
-public class AdiEndTerm {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int[] weight = new int[n];
-    int[] profit = new int[n];
-    for (int i = 0; i < n; i++) {
-      weight[i] = sc.nextInt();
-      profit[i] = sc.nextInt();
-    }
-    int capacity = sc.nextInt();
-    int[][] dp = new int[n + 1][capacity + 1];
-    for (int i = 0; i <= n; i++) {
-      for (int j = 0; j <= capacity; j++) {
-        if (i == 0 || j == 0) {
-          dp[i][j] = 0;
-        } else if (weight[i - 1] <= j) {
-          dp[i][j] = Math.max(profit[i - 1] + dp[i - 1][j - weight[i - 1]], dp[i - 1][j]);
-        } else {
-          dp[i][j] = dp[i - 1][j];
+// public class AdiEndTerm {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int n = sc.nextInt();
+//     int[] weight = new int[n];
+//     int[] profit = new int[n];
+//     for (int i = 0; i < n; i++) {
+//       weight[i] = sc.nextInt();
+//       profit[i] = sc.nextInt();
+//     }
+//     int capacity = sc.nextInt();
+//     int[][] dp = new int[n + 1][capacity + 1];
+//     for (int i = 0; i <= n; i++) {
+//       for (int j = 0; j <= capacity; j++) {
+//         if (i == 0 || j == 0) {
+//           dp[i][j] = 0;
+//         } else if (weight[i - 1] <= j) {
+//           dp[i][j] = Math.max(profit[i - 1] + dp[i - 1][j - weight[i - 1]], dp[i - 1][j]);
+//         } else {
+//           dp[i][j] = dp[i - 1][j];
 
-        }
-      }
-    }
-    System.out.println(dp[n][capacity]);
-  }
-}
+//         }
+//       }
+//     }
+//     System.out.println(dp[n][capacity]);
+//   }
+// }
 
 // Question 2: Password of the treasure box
 
