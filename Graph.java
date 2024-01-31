b61581d63c4704ad36270e4ddb9e92191fd5453d
Aditya <adityagaba1322@gmail.com>
2024-01-31 19:02:00 +0530

    a

--- Graph.java
+++ Graph.java
@@ -1352,107 +1352,107 @@
 
 // 2.) Bellman Ford Algorithm
 
-import java.util.*;
+// import java.util.*;
 
-class Graph {
-  class Edge {
-    int source;
-    int destination;
-    int weight;
+// class Graph {
+//   class Edge {
+//     int source;
+//     int destination;
+//     int weight;
 
-    Edge() {
-      source = 0;
-      destination = 0;
-      weight = 0;
-    }
+//     Edge() {
+//       source = 0;
+//       destination = 0;
+//       weight = 0;
+//     }
 
-    Edge(int source, int destination, int weight) {
-      this.source = source;
-      this.destination = destination;
-      this.weight = weight;
-    }
-  }
+//     Edge(int source, int destination, int weight) {
+//       this.source = source;
+//       this.destination = destination;
+//       this.weight = weight;
+//     }
+//   }
 
-  int V;
-  int E;
-  Edge edge[];
+//   int V;
+//   int E;
+//   Edge edge[];
 
-  Graph(int v, int e) {
-    V = v;
-    E = e;
-    edge = new Edge[E];
+//   Graph(int v, int e) {
+//     V = v;
+//     E = e;
+//     edge = new Edge[E];
 
-    for (int i = 0; i < E; i++) {
-      edge[i] = new Edge();
-    }
-  }
+//     for (int i = 0; i < E; i++) {
+//       edge[i] = new Edge();
+//     }
+//   }
 
-  void BellmanFord(Graph graph, int source) {
-    int V = graph.V;
-    int E = graph.E;
-    int dist[] = new int[V];
+//   void BellmanFord(Graph graph, int source) {
+//     int V = graph.V;
+//     int E = graph.E;
+//     int dist[] = new int[V];
 
-    // Step 1: Initialize distances from src to all other vertices as INFINITE
-    for (int i = 0; i < V; ++i)
-      dist[i] = Integer.MAX_VALUE;
-    dist[source] = 0;
+//     // Step 1: Initialize distances from src to all other vertices as INFINITE
+//     for (int i = 0; i < V; ++i)
+//       dist[i] = Integer.MAX_VALUE;
+//     dist[source] = 0;
 
-    // Step 2: Relax all edges |V| - 1 times. A simple shortest path from src to any
-    // other vertex can have at-most |V| - 1 edges
-    for (int i = 1; i < V; ++i) {
-      for (int j = 0; j < E; ++j) {
-        int u = graph.edge[j].source;
-        int v = graph.edge[j].destination;
-        int weight = graph.edge[j].weight;
-        if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v])
-          dist[v] = dist[u] + weight;
-      }
-    }
+//     // Step 2: Relax all edges |V| - 1 times. A simple shortest path from src to any
+//     // other vertex can have at-most |V| - 1 edges
+//     for (int i = 1; i < V; ++i) {
+//       for (int j = 0; j < E; ++j) {
+//         int u = graph.edge[j].source;
+//         int v = graph.edge[j].destination;
+//         int weight = graph.edge[j].weight;
+//         if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v])
+//           dist[v] = dist[u] + weight;
+//       }
+//     }
 
-    // Step 3: check for negative-weight cycles. The above step guarantees shortest
-    // distances if graph doesn't contain negative weight cycle. If we get a shorter
-    // path, then there is a cycle.
-    for (int j = 0; j < E; ++j) {
-      int u = graph.edge[j].source;
-      int v = graph.edge[j].destination;
-      int weight = graph.edge[j].weight;
-      if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
-        System.out.println("Graph contains negative weight cycle");
-        return;
-      }
-    }
-    printArr(dist, V);
-  }
+//     // Step 3: check for negative-weight cycles. The above step guarantees shortest
+//     // distances if graph doesn't contain negative weight cycle. If we get a shorter
+//     // path, then there is a cycle.
+//     for (int j = 0; j < E; ++j) {
+//       int u = graph.edge[j].source;
+//       int v = graph.edge[j].destination;
+//       int weight = graph.edge[j].weight;
+//       if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+//         System.out.println("Graph contains negative weight cycle");
+//         return;
+//       }
+//     }
+//     printArr(dist, V);
+//   }
 
-  // A utility function used to print the solution
-  void printArr(int dist[], int V) {
-    System.out.println("Vertex Distance from Source");
-    for (int i = 0; i < V; ++i)
-      System.out.println(i + "\t\t" + dist[i]);
-  }
+//   // A utility function used to print the solution
+//   void printArr(int dist[], int V) {
+//     System.out.println("Vertex Distance from Source");
+//     for (int i = 0; i < V; ++i)
+//       System.out.println(i + "\t\t" + dist[i]);
+//   }
 
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter the number of vertices:");
-    int v = scanner.nextInt();
-    System.out.println("Enter the number of edges:");
-    int e = scanner.nextInt();
+//   public static void main(String[] args) {
+//     Scanner scanner = new Scanner(System.in);
+//     System.out.println("Enter the number of vertices:");
+//     int v = scanner.nextInt();
+//     System.out.println("Enter the number of edges:");
+//     int e = scanner.nextInt();
 
-    Graph graph = new Graph(v, e);
+//     Graph graph = new Graph(v, e);
 
-    System.out.println("Enter the edges:");
-    for (int i = 0; i < e; i++) {
-      int src = scanner.nextInt();
-      int dest = scanner.nextInt();
-      int weight = scanner.nextInt();
-      graph.edge[i].source = src;
-      graph.edge[i].destination = dest;
-      graph.edge[i].weight = weight;
-    }
+//     System.out.println("Enter the edges:");
+//     for (int i = 0; i < e; i++) {
+//       int src = scanner.nextInt();
+//       int dest = scanner.nextInt();
+//       int weight = scanner.nextInt();
+//       graph.edge[i].source = src;
+//       graph.edge[i].destination = dest;
+//       graph.edge[i].weight = weight;
+//     }
 
-    graph.BellmanFord(graph, 0);
-  }
-}
+//     graph.BellmanFord(graph, 0);
+//   }
+// }
 
 // or
 
